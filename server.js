0cd6b8a3e3203e6c88daf455cdd91c84f8cea6fc
Alitodaroj <aroj34@yahoo.com>
2023-11-16 11:50:24 -0500

    mongoose installed

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 
+require('dotenv').config();
 const app = express();
 
 app.use(logger('dev'));
